648278b34329cd5f1ce6bff684e0716e3c09140f
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2022-08-29 02:16:46 -0500

    eaau123oE

--- app/src/main/java/com/zytronium/gravapult/MainActivity.kt
+++ app/src/main/java/com/zytronium/gravapult/MainActivity.kt
@@ -12,7 +12,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var planet: ImageView
     private var maxObstDist = 25
     private var grav: Force = Force(0f, 0f)
-    private var totalForce: Array<Force> = arrayOf(Force(40f, -30f, rightRot = 8f), grav)
+    private var velc = Force(10f, 0f, rightRot = 6f)
+    private var totalForce: Array<Force> = arrayOf(velc, grav)
     private var simulating = false
     private var startPoint: Coordinate = Coordinate(0f, 0f)
     private var found = false
@@ -25,6 +26,7 @@ class MainActivity : AppCompatActivity() {
         planet.setOnClickListener {
             simulating = !simulating
             if(simulating) {
+            velc = Force(10f, 0f, rightRot = 6f)
                 player.x = startPoint.x
                 player.y = startPoint.y
                 applyForces()
@@ -51,6 +53,8 @@ class MainActivity : AppCompatActivity() {
 //        Timer().scheduleAtFixedRate(1500, 500) {
             Handler().postDelayed({
         if(simulating) {
+            var x1 = player.x+(player.width/2)
+            var y1 = player.y+(player.height/2)
                 println(player.x.toString() + ", " + player.y.toString())
                 grav = gravity(player, planet)
                 println(grav.toString())
@@ -66,31 +70,37 @@ class MainActivity : AppCompatActivity() {
                     rot -= force.leftRot
                     rot += force.rightRot
                 }
-                player.x += totalX / 3
-                player.y += totalY / 3
-                player.rotation += rot
+                player.x += totalX
+                player.y += totalY
+                player.rotation += rot/2
                 println(player.x.toString() + ", " + player.y.toString())
+//            velc = Force(totalX/1.125f, totalY/-1.125f)
+                println(velc.toString())
+//                totalForce.component1().x = velc.x
+//                totalForce.component1().y = velc.y
+//                totalForce.component1().leftRot = velc.leftRot
+//                totalForce.component1().rightRot = velc.rightRot
                 applyForces()
             }
-        }, 50)
+        }, 25)
 //        } while(true)
 //    }
         }
 
     private fun gravity(obj1: View, obj2: View): Force {
-        var mass1 = obj1.width * obj1.height
-        var mass2 = obj2.width * obj2.height
-        var cent1 = Coordinate(obj1.x + (obj1.width/2), obj1.y -(obj1.height/2))
-        var cent2 = Coordinate(obj2.x + (obj2.width/2), obj2.y -(obj2.height/2))
-        var dist = kotlin.math.sqrt(
+        val mass1 = obj1.width * obj1.height
+        val mass2 = obj2.width * obj2.height
+        val cent1 = Coordinate(obj1.x + (obj1.width/2), obj1.y - (obj1.height/2))
+        val cent2 = Coordinate(obj2.x + (obj2.width/2), obj2.y - (obj2.height/2))
+        val dist = kotlin.math.sqrt(
                             kotlin.math.abs(cent1.x - cent2.x).toDouble().pow(2) + kotlin.math.abs(cent1.y - cent2.y)
                                 .toDouble().pow(2)
                         )
-        var reltvPos2 =  Coordinate(cent2.x-cent1.x, cent2.y+cent1.y)
-        var gc = 6.6742f
-        var gForce = gc*((mass1*mass2) / (dist*10).pow(2)).toFloat()/1.5f
+        val reltvPos2 =  Coordinate(cent2.x-cent1.x, cent1.y-cent2.y)
+        val gc = 6.6742f * 10.0.pow(-11).toFloat()
+        val gForce = (gc*((mass1*mass2) / (dist/500).pow(2)).toFloat())
         println("gforce: $gForce")
-        return Force(((reltvPos2.x)/(reltvPos2.x/2)) * if(reltvPos2.x < 0) gForce* -1 else gForce, ((reltvPos2.x)/(reltvPos2.y/2))*if(reltvPos2.y < 0) gForce* -1 else gForce)
+        return Force(((/*reltvPos2.x / */ (reltvPos2.x)) * if(reltvPos2.x < 0) (gForce * -1) else gForce), ((/*reltvPos2.x / */(reltvPos2.y)) * (if(reltvPos2.y < 0) (gForce * -1) else (gForce))))
 
 
     }
